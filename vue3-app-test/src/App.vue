ac03e6695429fc37e7f467ec2b216f7b4df12bc4
kaikaiaaaaa <1628928319@qq.com>
2022-08-17 00:16:40 +0800

    U update vue3组件联系

--- vue3-app-test/src/App.vue
+++ vue3-app-test/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.12 + Vite" />
+	<CheckEdit v-model="checked" v-model:title.trim="title"></CheckEdit>
+	{{checked}}
+	{{title}}
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import CheckEdit from './components/testComps/CheckEdit.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+		CheckEdit
+  },
+	data(){
+  	return{
+  		checked:false,
+			title:'111111'
+		}
+	},
 }
 </script>
---
+++ vue3-app-test/src/components/testComps/CheckEdit.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="wrapper">
+		<div class="checkbox" :class="{checked:modelValue}" @click="handleChecked"></div>
+		<input type="text" :value="title" @input="handleInput">
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CheckEdit",
+	props: {
+		modelValue: Boolean,
+		title: String,
+		titleModifiers: {
+			default: () => {
+				{
+				}
+			}
+		}
+	},
+	setup(props, ctx) {
+		const handleChecked = () => {
+			console.log('ctx', ctx);
+			ctx.emit("update:modelValue", !props.modelValue)
+		}
+		const handleInput = (e) => {
+			//修饰符去掉空格
+			props.titleModifiers.trim ? e.target.value = e.target.value.trim() : null
+			ctx.emit("update:title", e.target.value)
+		}
+		return {
+			handleChecked,
+			handleInput
+		}
+	},
+}
+</script>
+
+<style scoped>
+.wrapper {
+	display: flex;
+
+}
+
+.checkbox {
+	width: 12px;
+	cursor: pointer;
+	margin-right: 2px;
+	height: 12px;
+	border: 1px solid;
+}
+
+.checked {
+	background-color: red;
+}
+
+</style>
